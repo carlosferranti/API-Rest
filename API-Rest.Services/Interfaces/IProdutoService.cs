3d87908ef52b3316ddf08e525010259f26466054
Carlos Silva <carlosferranti@gmail.com>
2022-09-03 19:58:08 -0300

    Add project files.

---
+++ API-Rest.Services/Interfaces/IProdutoService.cs
@@ -0,0 +1,19 @@
+﻿using API_Rest.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API_Rest.Services
+{
+    interface IProdutoService
+    {       
+        Task<IEnumerable<Produto>> GetAllProduto();
+
+        Task<Produto> GetProdutoById(int id);
+        Task<Produto> GetProdutoByNome(string nome);              
+        void AddProdutos(Produto produto);
+        void UpdateProduto(Produto produto);
+        void DeleteProduto(int id);
+    }
+}
